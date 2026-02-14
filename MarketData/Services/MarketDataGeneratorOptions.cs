e67458c907f2942bacf7e4326908ad4ec4980f39
szigh <jonasdhalling@gmail.com>
2026-02-14 21:09:38 +0000

    Allow throttling of database persistance & grpc publishing in appsettings

--- MarketData/Services/MarketDataGeneratorOptions.cs
+++ MarketData/Services/MarketDataGeneratorOptions.cs
@@ -5,4 +5,6 @@ public class MarketDataGeneratorOptions
     public const string SectionName = "MarketDataGenerator";
 
     public int CheckIntervalMilliseconds { get; set; } = 100;
+    public int DatabasePersistenceMilliseconds { get; set; } = 10000;
+    public int GrpcPublishMilliseconds { get; set; } = 100;
 }
