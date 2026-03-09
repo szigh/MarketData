54c9398a271cfa5b1465442f38f2c87ba74d5766
szigh <szjonas4@gmail.com>
2026-03-09 13:37:18 +0000

    unnecessary usings; formatting

--- MarketData/Services/DefaultModelConfigFactory.cs
+++ MarketData/Services/DefaultModelConfigFactory.cs
@@ -1,60 +1,58 @@
 ﻿using MarketData.Models;
-using System.Diagnostics.Metrics;
 using System.Text.Json;
 
-namespace MarketData.Services
+namespace MarketData.Services;
+
+/// <summary>
+/// Utility class to create default model configurations.
+/// The values are deliberately arbitrary.
+/// </summary>
+internal static class DefaultModelConfigFactory
 {
-    /// <summary>
-    /// Utility class to create default model configurations.
-    /// The values are deliberately arbitrary.
-    /// </summary>
-    internal static class DefaultModelConfigFactory
+    public static FlatConfig CreateFlatConfig(int instrumentId)
     {
-        public static FlatConfig CreateFlatConfig(int instrumentId)
+        return new FlatConfig
         {
-            return new FlatConfig
-            {
-                InstrumentId = instrumentId
-            };
-        }
+            InstrumentId = instrumentId
+        };
+    }
 
-        public static RandomMultiplicativeConfig CreateRandomMultiplicativeConfig(int instrumentId)
+    public static RandomMultiplicativeConfig CreateRandomMultiplicativeConfig(int instrumentId)
+    {
+        return new RandomMultiplicativeConfig
         {
-            return new RandomMultiplicativeConfig
-            {
-                InstrumentId = instrumentId,
-                StandardDeviation = 0.00388, // default: 99% within 1%
-                Mean = 0.0
-            };
-        }
+            InstrumentId = instrumentId,
+            StandardDeviation = 0.00388, // default: 99% within 1%
+            Mean = 0.0
+        };
+    }
 
-        public static MeanRevertingConfig CreateMeanRevertingConfig(int instrumentId, double mean = 100d)
+    public static MeanRevertingConfig CreateMeanRevertingConfig(int instrumentId, double mean = 100d)
+    {
+        return new MeanRevertingConfig
         {
-            return new MeanRevertingConfig
-            {
-                InstrumentId = instrumentId,
-                Mean = mean,
-                Kappa = 0.0004,
-                Sigma = 0.5,
-                Dt = 0.1
-            };
-        }
+            InstrumentId = instrumentId,
+            Mean = mean,
+            Kappa = 0.0004,
+            Sigma = 0.5,
+            Dt = 0.1
+        };
+    }
 
-        public static RandomAdditiveWalkConfig CreateRandomAdditiveWalkConfig(int instrumentId)
+    public static RandomAdditiveWalkConfig CreateRandomAdditiveWalkConfig(int instrumentId)
+    {
+        var walkSteps = new[]
         {
-            var walkSteps = new[]
-{
-                    new { Probability = 0.25, Value = -0.01 },
-                    new { Probability = 0.25, Value = -0.005 },
-                    new { Probability = 0.25, Value = 0.005 },
-                    new { Probability = 0.25, Value = 0.01 }
-                };
+            new { Probability = 0.25, Value = -0.01 },
+            new { Probability = 0.25, Value = -0.005 },
+            new { Probability = 0.25, Value = 0.005 },
+            new { Probability = 0.25, Value = 0.01 }
+        };
 
-            return new RandomAdditiveWalkConfig
-            {
-                InstrumentId = instrumentId,
-                WalkStepsJson = JsonSerializer.Serialize(walkSteps)
-            };
-        }
+        return new RandomAdditiveWalkConfig
+        {
+            InstrumentId = instrumentId,
+            WalkStepsJson = JsonSerializer.Serialize(walkSteps)
+        };
     }
 }
