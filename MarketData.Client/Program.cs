e66fd0f7f799018f792d8d2d3e8478aef9549807
szigh <szjonas4@gmail.com>
2026-03-16 07:52:54 +0000

    Added seq and serilog to console logging

--- MarketData.Client/Program.cs
+++ MarketData.Client/Program.cs
@@ -1,6 +1,7 @@
 using MarketData.Client;
 using MarketData.Client.Shared.Configuration;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 internal class Program
 {
@@ -11,47 +12,86 @@ internal class Program
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .Build();
 
-        var grpcSettings = configuration.GetSection(GrpcSettings.SectionName)
-            .Get<GrpcSettings>() ?? new GrpcSettings();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
 
-        var modelConfigClient = new GrpcModelConfigClient(grpcSettings);
-        var priceStreamer = new PriceStreamer(grpcSettings);
-
-        while (true)
+        try
         {
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine($"Press (Ctrl+C) to exit.");
-            Console.WriteLine();
+            LogBanner();
+            Log.Information("Starting Console Market Data Client");
 
-            var availableInstruments = await modelConfigClient.GetConfiguredInstruments();
-            Console.WriteLine($"Available instruments: {string.Join(", ", availableInstruments)}");
-            
-            var sep = new string('=', 30);
-            Console.WriteLine($"Menu {sep}");
-            Console.WriteLine($"1. Add instrument");
-            Console.WriteLine($"2. Remove instrument");
-            Console.WriteLine($"3. View configurations");
-            Console.WriteLine($"4. Start price streaming");
+            var grpcSettings = configuration.GetSection(GrpcSettings.SectionName)
+                .Get<GrpcSettings>() ?? new GrpcSettings();
 
-            Console.Write($">>> ");
-            var input = Console.ReadLine();
-            if(input == "1")
-            {
-                await modelConfigClient.AddInstrument();
+            var modelConfigClient = new GrpcModelConfigClient(grpcSettings);
+            var priceStreamer = new PriceStreamer(grpcSettings);
+
+                    while (true)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine($"Press (Ctrl+C) to exit.");
+                        Console.WriteLine();
+
+                        var availableInstruments = await modelConfigClient.GetConfiguredInstruments();
+                        Console.WriteLine($"Available instruments: {string.Join(", ", availableInstruments)}");
+
+                        var sep = new string('=', 30);
+                        Console.WriteLine($"Menu {sep}");
+                        Console.WriteLine($"1. Add instrument");
+                        Console.WriteLine($"2. Remove instrument");
+                        Console.WriteLine($"3. View configurations");
+                        Console.WriteLine($"4. Start price streaming");
+
+                        Console.Write($">>> ");
+                        var input = Console.ReadLine();
+                        if(input == "1")
+                        {
+                            await modelConfigClient.AddInstrument();
+                        }
+                        else if (input == "2")
+                        {
+                            await modelConfigClient.RemoveInstrument();
+                        }
+                        else if (input == "3")
+                        {
+                            await modelConfigClient.GetConfiguredInstruments(printConfigs: true);
+                        }
+                        else if (input == "4")
+                        {
+                            await priceStreamer.Start();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Fatal(ex, "Application terminated unexpectedly");
+                    throw;
+                }
+                finally
+                {
+                    Log.Information("Shutting down Console Market Data Client");
+                    Log.CloseAndFlush();
+                }
             }
-            else if (input == "2")
+
+            private static void LogBanner()
             {
-                await modelConfigClient.RemoveInstrument();
-            }
-            else if (input == "3")
-            {
-                await modelConfigClient.GetConfiguredInstruments(printConfigs: true);
-            }
-            else if (input == "4")
-            {
-                await priceStreamer.Start();
+                const string banner =
+        @"
+          __  __            _        _       _       _        
+         |  \/  |          | |      | |     | |     | |       
+         | \  / | __ _ _ __| | _____| |_  __| | __ _| |_ __ _ 
+         | |\/| |/ _` | '__| |/ / _ \ __|/ _` |/ _` | __/ _` |
+         | |  | | (_| | |  |   <  __/ |_| (_| | (_| | || (_| |
+         |_|  |_|\__,_|_|  |_|\_\___|\__|\__,_|\__,_|\__\__,_|
+           ____                      _        
+          / ___|___  _ __  ___  ___ | | ___   
+         | |   / _ \| '_ \/ __|/ _ \| |/ _ \  
+         | |__| (_) | | | \__ \ (_) | |  __/  
+          \____\___/|_| |_|___/\___/|_|\___|  
+                                              ";
+                Log.Logger.Information(banner);
             }
         }
-    }
-}
